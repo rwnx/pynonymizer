d32d82c16b2a9bea4f6d6bb574ff8e10728cbb06
Jerome Twell <jtwell1@gmail.com>
2019-06-10 01:44:45 +0100

    add fallbacks for positionals and sys.exits

--- pynonymizer/__main__.py
+++ pynonymizer/__main__.py
@@ -19,39 +19,43 @@ def create_parser():
     parser = argparse.ArgumentParser(description="\n".join([
     "A tool for writing better anonymization strategies for your production databases.",
     ]))
-    parser.add_argument("input", help="The source dumpfile to read from. \n[.sql, .gz]")
-    parser.add_argument("strategyfile", help="A strategyfile to use during anonymization.")
-    parser.add_argument("output", help="The destination to write the dumped output to. \n[.sql, .gz]")
-    parser.add_argument("--db-type", "-t", default=None, required=False, help="database type.")
-    parser.add_argument("--db-host", "-d", default=None, required=False, help="database host")
-    parser.add_argument("--db-name", "-n", default=None, required=False, help="Name of database to create in the target host and restore to. This will default to a random name.")
-    parser.add_argument("--db-user", "-u", default=None, required=False, help="Database username.")
-    parser.add_argument("--db-password", "-p", default=None, required=False, help="Database password.")
-    parser.add_argument("--fake-locale", "-l", default=None, required=False, help="locale to generate fake data for.")
+    parser.add_argument("input", default=None, help="The source dumpfile to read from. \n[.sql, .gz]")
+    parser.add_argument("strategyfile", default=None, help="A strategyfile to use during anonymization.")
+    parser.add_argument("output", default=None, help="The destination to write the dumped output to. \n[.sql, .gz]")
+    parser.add_argument("--db-type", "-t", default=None, help="database type.")
+    parser.add_argument("--db-host", "-d", default=None, help="database host")
+    parser.add_argument("--db-name", "-n", default=None, help="Name of database to create in the target host and restore to. This will default to a random name.")
+    parser.add_argument("--db-user", "-u", default=None, help="Database username.")
+    parser.add_argument("--db-password", "-p", default=None, help="Database password.")
+    parser.add_argument("--fake-locale", "-l", default=None, help="locale to generate fake data for.")
     parser.add_argument("-v", "--version", action="version", version=__version__)
 
     return parser
 
 def main(args=None):
     parser = create_parser()
-    args = parser.parse_args(args)
+    parsed_args = parser.parse_args(args)
 
     # find the dotenv from the current working dir rather than the execution location
     dotenv = find_dotenv(usecwd=True)
     load_dotenv(dotenv_path=dotenv)
 
-    db_type = args.db_type or os.getenv("DB_TYPE") or "mysql"
-    db_host = args.db_host or os.getenv("DB_HOST") or "127.0.0.1"
-    db_name = args.db_name or os.getenv("DB_NAME") or get_temp_db_name(args.strategyfile)
-    db_user = args.db_user or os.getenv("DB_USER") or sys.exit("Missing environment variable: DB_USER")
-    db_pass = args.db_password or os.getenv("DB_PASS") or sys.exit("Missing environment variable: DB_PASS")
-    fake_locale = args.fake_locale or os.getenv("FAKE_LOCALE") or "en_GB"
+    input_path = parsed_args.input or os.getenv("PYNON_INPUT") or sys.exit("Missing argument: [input]/PYNON_INPUT")
+    strategyfile_path = parsed_args.strategyfile or os.getenv("PYNON_STRATEGYFILE") or sys.exit("Missing argument: [strategyfile]/PYNON_STRATEGYFILE")
+    output_path = parsed_args.output or os.getenv("PYNON_OUTPUT") or sys.exit("Missing argument: [output]/PYNON_OUTPUT")
+
+    db_type = parsed_args.db_type or os.getenv("PYNON_DB_TYPE") or os.getenv("DB_TYPE") or "mysql"
+    db_host = parsed_args.db_host or os.getenv("PYNON_DB_HOST") or os.getenv("DB_HOST") or "127.0.0.1"
+    db_name = parsed_args.db_name or os.getenv("PYNON_DB_NAME") or os.getenv("DB_NAME") or get_temp_db_name(parsed_args.strategyfile)
+    db_user = parsed_args.db_user or os.getenv("PYNON_DB_USER") or os.getenv("DB_USER") or sys.exit("Missing argument: --db-user/PYNON_DB_USER")
+    db_pass = parsed_args.db_password or os.getenv("PYNON_DB_PASSWORD") or os.getenv("DB_PASS") or sys.exit("Missing argument: --db-password/PYNON_DB_PASSWORD")
+    fake_locale = parsed_args.fake_locale or os.getenv("PYNON_FAKE_LOCALE") or os.getenv("FAKE_LOCALE") or "en_GB"
 
     fake_seeder = FakeColumnSet(fake_locale)
     strategy_parser = StrategyParser(fake_seeder)
 
-    logger.debug("loading strategyfile %s", args.strategyfile)
-    with open(args.strategyfile, "r") as strategy_yaml:
+    logger.debug("loading strategyfile %s", strategyfile_path)
+    with open(strategyfile_path, "r") as strategy_yaml:
         strategy = strategy_parser.parse_config(yaml.safe_load(strategy_yaml))
 
     # init and validate DB connection
@@ -62,8 +66,8 @@ def main(args=None):
         sys.exit("Unable to connect to database.")
 
     # locate i/o
-    input_obj = input.from_location(args.input)
-    output_obj = output.from_location(args.output)
+    input_obj = input.from_location(input_path)
+    output_obj = output.from_location(output_path)
     logger.debug("input: %s output: %s", input_obj, output_obj)
 
     # main process
