9d9d8591eeb9c9de4b99a8fca5c860fe34483c5d
Jerome Twell <jtwell1@gmail.com>
2021-03-27 00:28:42 +0000

    add pypi description for py39

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     author='Jerome Twell',
     author_email='jtwell1@gmail.com',
