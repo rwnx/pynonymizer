fa145126b72c2b6da98d9231c548d1ffd8d66a97
Jerome Twell <jtwell1@gmail.com>
2021-05-06 15:14:04 +0100

    consolidate mssql script, change +e mode for test

--- tests_integration/mssql/install_mssql.sh
+++ tests_integration/mssql/install_mssql.sh
@@ -7,10 +7,8 @@ MSSQL_PID='developer'
 
 echo Adding Microsoft repositories...
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-repoargs="$(curl https://packages.microsoft.com/config/ubuntu/$DISTRIB_RELEASE/mssql-server-2019.list)"
-sudo add-apt-repository "${repoargs}"
-repoargs="$(curl https://packages.microsoft.com/config/ubuntu/$DISTRIB_RELEASE/prod.list)"
-sudo add-apt-repository "${repoargs}"
+sudo add-apt-repository "$(curl https://packages.microsoft.com/config/ubuntu/$DISTRIB_RELEASE/mssql-server-2019.list)"
+sudo add-apt-repository "$(curl https://packages.microsoft.com/config/ubuntu/$DISTRIB_RELEASE/prod.list)"
 
 echo Running apt-get update -y...
 sudo apt-get update -y
@@ -19,9 +17,7 @@ echo Installing SQL Server...
 sudo apt-get install -y mssql-server
 
 echo Running mssql-conf setup...
-sudo MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD \
-     MSSQL_PID=$MSSQL_PID \
-     /opt/mssql/bin/mssql-conf -n setup accept-eula
+sudo /opt/mssql/bin/mssql-conf -n setup accept-eula
 
 echo Installing mssql-tools and unixODBC developer...
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
@@ -32,8 +28,11 @@ echo PATH="$PATH:/opt/mssql-tools/bin" >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 
-echo manual starting SQL Server...
+echo Starting SQL Server...
 sudo service mssql-server start 
+
+# Allow err in sqlcmd for test steps
+set +e
 
 # Connect to server and get the version:
 counter=1
@@ -53,30 +52,8 @@ done
 
 if [ $errstatus = 1 ]
 then
-  echo trying to start mssql again...
-  sudo service mssql-server start 
-
-  # Connect to server and get the version:
-  counter=1
-  errstatus=1
-  while [ $counter -le 20 ] && [ $errstatus = 1 ]
-  do
-    echo Waiting for SQL Server to start...
-    sleep 3s
-    /opt/mssql-tools/bin/sqlcmd \
-      -S localhost \
-      -U SA \
-      -P $MSSQL_SA_PASSWORD \
-      -Q "SELECT @@VERSION" 2>/dev/null
-    errstatus=$?
-    ((counter++))
-  done
-
-  if [ $errstatus = 1 ]
-  then
-    echo Cannot connect to SQL Server, installation aborted
-    exit $errstatus
-  fi
+  echo Cannot connect to SQL Server, installation aborted
+  exit $errstatus
 fi
 
 echo Done!
